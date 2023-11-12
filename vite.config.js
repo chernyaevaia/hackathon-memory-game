3657395b9d68b0f5efb6d8e098ccd424bc3da594
Ирина Черняева <iridache@192.168.1.4>
2023-11-12 16:45:59 +0300

    vite config fixed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: base: "/family-memory-game/"
+  base:"/family-memory-game/",
   plugins: [react()],
 })
